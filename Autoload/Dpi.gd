f7e82504d4643543470052105cd58894d53e636e
toger5 <toger5@hotmail.de>
2018-06-25 02:16:36 +0200

    dpi class screen size

--- Autoload/Dpi.gd
+++ Autoload/Dpi.gd
@@ -2,9 +2,13 @@ extends Node
 
 var font_size_cm = 0.4
 var screen_dpi = OS.get_screen_dpi(0)
+var screen_size setget ,_get_screen_size
 func _ready():
 	var theme = load("res://theme/table.theme")
 	theme.default_font.size = cm_to_pixel(font_size_cm)
 
 func cm_to_pixel(cm):
 	return screen_dpi * cm/2.5
+
+func _get_screen_size():
+	return get_tree().get_root().size
